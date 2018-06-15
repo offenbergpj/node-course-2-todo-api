0b6ff44b7b47422dc76d06277288181b322cb7a6
Patrick <p.offenberg@gmx.net>
2018-06-15 15:53:22 +0200

    Add Delete route

--- server/server.js
+++ server/server.js
@@ -51,6 +51,34 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+
+    // get id
+    // validate id. 
+        // if not valid. return 404
+    // remove todo by id
+        // succes
+            // if no doc send 404
+            // if doc, send doc with 200
+    // error. 
+        //400 with empty body
+
+    var id = req.params.id;
+
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({ todo });
+    }).catch((e) => res.status(400).send());
+
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
