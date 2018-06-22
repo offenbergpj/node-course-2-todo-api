57a81d3ce425a2243e10ce48aa124c4b10cd1c85
Patrick <p.offenberg@gmx.net>
2018-06-22 08:49:49 +0200

    add test for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -133,7 +133,7 @@ app.delete('/users/me/token', authenticate, (req, res) => {
         res.status(200).send();
     }, () => {
         res.status(400).send();
-    })
+    });
 });
 
 app.get('/users/me',authenticate, (req, res) => {
